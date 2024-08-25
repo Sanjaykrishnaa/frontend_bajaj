50409e70083f585a112ffb27b632b26603f535aa
Sanjaykrishnaa <108727040+Sanjaykrishnaa@users.noreply.github.com>
2024-08-25 14:49:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-
+import './App.css';
 const App = () => {
   const [input, setInput] = useState('');
   const [response, setResponse] = useState(null);   
